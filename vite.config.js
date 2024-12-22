23eaa34427a724d2f34050a2d85502d3c8feeadd
Dbrandow332 <119837275+Dbrandow332@users.noreply.github.com>
2024-12-22 14:46:47 -0500

    Fix Rollup external dependency issue for react-chartjs-2

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ import path from 'path';
 
 export default defineConfig({
     plugins: [react()],
+    build: {
+        rollupOptions: {
+            external: ['react-chartjs-2', 'chart.js'],
+        }
+    },
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src'),
